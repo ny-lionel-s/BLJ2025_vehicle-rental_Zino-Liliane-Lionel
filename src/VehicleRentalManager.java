79394de04e4b63dc5b53f295e9d48941932cfcba
ny-lionel-s <lionel.schultze@noseryoung.ch>
2026-05-20 12:11:06 +0200

    made print functions for the lists

--- src/VehicleRentalManager.java
+++ src/VehicleRentalManager.java
@@ -7,7 +7,6 @@ public class VehicleRentalManager {
     private final ArrayList<Contract> contracts;
 
 
-
     public VehicleRentalManager() {
         customerList = new ArrayList<>();
         denyList = new ArrayList<>();
@@ -16,18 +15,45 @@ public class VehicleRentalManager {
     }
 
 
-    void addPersonToDenylist(Person p){
+    void addPersonToDenylist(Person p) {
         denyList.add(p);
     }
 
-    void createContract(){
+    void createContract() {
 
     }
 
+    void addPersonToCustomerList(Person p) {
+        customerList.add(p);
+    }
 
+    void addVehicleToVehicles(Vehicle v) {
+        vehicles.add(v);
+    }
 
+    void printCustomerList() {
+        for (int i = 0; i < customerList.size(); i++) {
+            System.out.println(customerList.get(i).getLastName());
+            System.out.println(customerList.get(i).getFirstName());
+            System.out.println(customerList.get(i).getBirthYear());
+        }
+    }
 
+    void printVehicles() {
+        for (int i = 0; i < vehicles.size(); i++) {
+            System.out.println(vehicles.get(i).getLastName());
+            System.out.println(vehicles.get(i).getFirstName());
+            System.out.println(vehicles.get(i).getBirthYear());
+        }
+    }
 
+    void printContractList() {
+        for (int i = 0; i < contracts.size(); i++) {
+            System.out.println(contracts.get(i).getLastName());
+            System.out.println(contracts.get(i).getFirstName());
+            System.out.println(contracts.get(i).getBirthYear());
+        }
+    }
 
 
     public ArrayList<Person> getCustomerList() {
