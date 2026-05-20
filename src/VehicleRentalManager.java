4648ca18c856174821cff1d93b0a1404296f79e8
ny-lionel-s <lionel.schultze@noseryoung.ch>
2026-05-20 11:46:52 +0200

    made fields and Getters

--- src/VehicleRentalManager.java
+++ src/VehicleRentalManager.java
@@ -1,2 +1,48 @@
+import java.util.ArrayList;
+
 public class VehicleRentalManager {
+    private final ArrayList<Person> customerList;
+    private final ArrayList<Person> denyList;
+    private final ArrayList<Vehicle> vehicles;
+    private final ArrayList<Contract> contracts;
+
+
+
+    public VehicleRentalManager() {
+        customerList = new ArrayList<>();
+        denyList = new ArrayList<>();
+        vehicles = new ArrayList<>();
+        contracts = new ArrayList<>();
+    }
+
+
+    void addPersonToDenylist(Person p){
+        denyList.add(p);
+    }
+
+    void createContract(){
+
+    }
+
+
+
+
+
+
+
+    public ArrayList<Person> getCustomerList() {
+        return customerList;
+    }
+
+    public ArrayList<Person> getDenyList() {
+        return denyList;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public ArrayList<Contract> getContracts() {
+        return contracts;
+    }
 }
