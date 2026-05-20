eba7b585751a273c1bec88c66b3f558082279d0b
zinoos <zinos.dao@gmail.com>
2026-05-20 11:25:54 +0200

    Added Fields for Person, Setter/Getters and added toString to make Fields readable

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,46 @@
+import java.time.LocalDate;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthYear;
+
+    public Person(String firstName, String lastName, LocalDate birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(LocalDate birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
 }
